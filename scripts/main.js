c9d1115dfefa111c5ed47b4eb5722d63ba6a563d
JustinLung <justinlung77@gmail.com>
2022-02-15 21:16:05 +0100

    WIP market section

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,62 @@
-const nav = document.querySelector('.nav')
-const menu = document.querySelector('.menu')
+// Variables
+const nav = document.querySelector('.nav');
+const menu = document.querySelector('.menu');
+
+const errorMsg = document.querySelector(".error");
+const nameEl = document.querySelector('.name');
 
 menu.addEventListener('click', () => {
-	menu.classList.toggle('active')
-	nav.classList.toggle('active')
-})
+	menu.classList.toggle('active');
+	nav.classList.toggle('active');
+});
 
 getData();
 
+// Functions
 async function getData() {
 	try {
-		const request = await fetch("https://tribe.api.fdnd.nl/v1/list");
-		const response = await request.json()
-		return response.data;
+		const request = await fetch("https://tribe.api.fdnd.nl/v1/member");
+		const response = await request.json();
+		renderData(response.data);
 	} catch (err) {
-		console.log(err);
+		errorMessage();
+		throw new Error(err);
 	}
 }
 
-async function render() {
-	//Sla data in variablen op
-	const data = await getData();
-	console.log(data);
-	//Map de array zodat alle namen uppercase zijn
-	// const upperCaseName = data.map(person => person.name.toUpperCase());
-	//Filter op de array voor iedereen waarbij de naam begint met een H
-	const sort = data.filter(tribe => tribe.tribeName == "FDND Founders")
-	sort.forEach(tribe => {
-		const personEl = Object.assign(document.createElement('p'), {
-			textContent: tribe.memberName
-		})
-		document.querySelector("main").appendChild(personEl)
-	})
-	console.log(sort);
+async function renderData(members) {
+	const memberFilter = members.filter(member => member.squadId == 1)
+	for (let i = 0; i < 6; i++) {
+		document.querySelector(".market-container").insertAdjacentHTML(
+			"afterbegin",
+			`          
+			<div class="card">
+            <figure>
+              <img src="${memberFilter[i].avatar}" alt="Profile Picture" class="card-image"/>
+            </figure>
+            <div class="card-header">
+              <div class="name-container">
+                <p>${memberFilter[i].type}</p>
+                <p class="name">${memberFilter[i].name}</p>
+              </div>
+              <div class="price-container">
+                <p>Price</p>
+                <p>1.2</p>
+              </div>
+            </div>
+            <a href="#">Buy Now</a>
+          </div>`
+		);
+		if (memberFilter[i].avatar === "") document.querySelector(".card-image").src = "../assets/not-available.png";
+	}
 }
-
-render()
+function errorMessage() {
+	setTimeout(() => {
+		errorMsg.style.opacity = 1,
+			errorMsg.style.transform = "translateX(0)";
+		setTimeout(() => {
+			errorMsg.style.opacity = 0,
+				errorMsg.style.transform = "translateX(4em)";
+		}, 5000)
+	}, 300)
+}
